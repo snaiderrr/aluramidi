cb1762246ac8ba44958a53f60ac6fdb9771f41ad
snaiderrr <filipekeven@hotmail.com>
2025-05-22 01:22:19 -0300

    Colocando som nas teclas

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -7,12 +7,13 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 let contador = 0;
 
 //enquanto
-while (contador < 9) {
-    listaDeTeclas[contador].onclick = tocaSomPom;
+while (contador < listaDeTeclas.length) {
+    listaDeTeclas[contador].onclick = function () {
+        tocaSom('#som_tecla_pom');
+    }
     
     contador = contador + 1;
 
     console.log(contador);
-
 
 }
